b45d698baad91ed68d7263ab0c7976cd71ab3402
Jackie <jackienorstrom@aol.com>
2018-10-08 10:22:58 -0400

    Initial Commit

---
+++ FinalProjectDeath/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FinalProjectDeath
+//
+//  Created by Jackie on 10/8/18.
+//  Copyright © 2018 LAS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
